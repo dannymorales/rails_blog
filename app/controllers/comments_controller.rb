75f3e9613c090b045552934cf755cbabbf7daee5
Danilo Morales <danny@gadaproductions.com>
2015-05-11 14:35:42 -0400

    more changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,12 @@
 class CommentsController < ApplicationController
-	before_filter :require_permission, only :edit
+	before_filter :require_permission, only: :edit
 
+	def new
+		@comments = current_user.comments.build
+	end
 	def create
 		@post = Post.find(params[:post_id])
-		@comment = @post.comments.create(params[:comment].permit(:name, :body))
+		@comment = @post.comments.create(comment_params)
 		redirect_to post_path(@post)
 	end
 
@@ -17,19 +20,25 @@ class CommentsController < ApplicationController
 
 	def show
 		@comment = Comment.find(params[:id])
-		@comment.user
+		@comment.user = current_user
 	end
 	def edit
 		@comment = Comment.find(params[:id])
+		@comment.user
 	end
 
 	def update
 		@comment = Comment.find(params[:id])
-		if @comment.update	
-		
+		@comment.update	
 	end
 	def require_permission
 		if current_user != Comment.find(params[:id]).user
-		redirect_to root_path		
+		redirect_to root_path
+		end	
 	end
+
+	private
+    def comment_params
+      params.require(:comment).permit(:body).merge(:user_id => current_user.id)
+    end 
 end
