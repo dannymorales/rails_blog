92616d9cf30c679db82058b724634c81d3e20442
Danilo Morales <danny@gadaproductions.com>
2015-05-13 16:40:47 -0400

    changes before adding cancan gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
 
-	# def index
-	# 	@posts = current_user.posts
-	# end
+	def index
+		@users = User.all.order("created_at ASC")
+	end
 
 	# def show
 	# 	@user = User.find(params[:id])
 	# end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to root_path
+	end
 end
